6f245333883fca9c2448d4b2beab68de427f4966
Marlon Leoner <marlon.leoner@gmail.com>
2021-02-03 13:27:16 -0300

    captura de exception

--- src/main/java/br/com/ufsm/usuario/api/handler/ErrorHandler.java
+++ src/main/java/br/com/ufsm/usuario/api/handler/ErrorHandler.java
@@ -7,12 +7,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import br.com.ufsm.usuario.api.controller.form.LoginForm;
 import br.com.ufsm.usuario.api.exceptions.ObjectAlreadyExistsException;
 import br.com.ufsm.usuario.api.exceptions.ObjectNotFoundException;
 import br.com.ufsm.usuario.api.model.Error;
@@ -31,14 +34,31 @@ public class ErrorHandler {
 
 	@ResponseStatus(code = HttpStatus.NOT_FOUND)
 	@ExceptionHandler(ObjectNotFoundException.class)
-	public Error objectNotFoundExceptionHandle(ObjectNotFoundException ex) {
+	public Error handleObjectNotFoundException(ObjectNotFoundException ex) {
 		return new Error("ObjectNotFound", 404, ex.getMessage());
 	}
 
 	@ResponseStatus(code = HttpStatus.BAD_REQUEST)
 	@ExceptionHandler(ObjectAlreadyExistsException.class)
-	public Error objectAlreadyExistsExceptionHandle(ObjectAlreadyExistsException ex) {
+	public Error handleObjectAlreadyExistsException(ObjectAlreadyExistsException ex) {
 		return new Error("ObjectAlreadyExists", 400, ex.getMessage());
+	}
+
+	@ResponseStatus(code = HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(BadCredentialsException.class)
+	public Error handleBadCredentialsException(BadCredentialsException ex) {
+		return new Error("BadCredentials", 400, ex.getMessage());
+	}
+
+	@ResponseStatus(code = HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(HttpMessageNotReadableException.class)
+	public Error handleHttpMessageNotReadableException(HttpMessageNotReadableException ex) {
+		String origin = ex.getMessage().split("[(]")[1].replace(")", "");
+		if (origin.equals(LoginForm.class.getName())) {
+			return new Error("MissingCredentials", 400, "Por favor, preencha seu e-mail e senha.");
+		}
+
+		return new Error("HttpMessageNotReadable", 400, ex.getMessage());
 	}
 
 	@ResponseStatus(code = HttpStatus.BAD_REQUEST)
