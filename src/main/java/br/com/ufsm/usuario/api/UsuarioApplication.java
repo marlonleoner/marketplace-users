bb8215698997bd793e1a3b076f711124a5d660c7
Marlon Leoner <marlon.leoner@gmail.com>
2021-02-03 12:57:17 -0300

    refatoração do pacote

--- src/main/java/br/com/ufsm/usuario/api/UsuarioApplication.java
+++ src/main/java/br/com/ufsm/usuario/api/UsuarioApplication.java
@@ -1,4 +1,4 @@
-package br.com.ufsm.usuarioApi;
+package br.com.ufsm.usuario.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
