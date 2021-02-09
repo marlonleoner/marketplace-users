7e828e94215c487135c20aeb3e225cc0dd12e646
fernandaBonaldo <74841732+fernandaBonaldo@users.noreply.github.com>
2021-02-09 16:02:20 -0300

    Create V003__insert_users.sql

---
+++ src/main/resources/db/migration/V003__insert_users.sql
@@ -0,0 +1,8 @@
+INSERT INTO user
+	(email, password, first_name, last_name, cpf, id, created_at, updated_at)
+VALUES
+	("william1@gmail.com", "$2a$10$wAWbBhyXnJBtQjUpZbDreuTvARojppXD319dMB050LHyAzyXE5hlW", "William Felipe", "de Almeida Borges", "66682414099", 1, GETDATE(), GETDATE()),
+	("pedro2@gmail.com", "$2a$10$wAWbBhyXnJBtQjUpZbDreuTvARojppXD319dMB050LHyAzyXE5hlW", "Pedro", "Hasan Carara", "06359771098", 2, GETDATE(), GETDATE()),
+	("vitoria3@gmail.com", "$2a$10$wAWbBhyXnJBtQjUpZbDreuTvARojppXD319dMB050LHyAzyXE5hlW", "Vitória", "Lik Santos", "58062550001", 3, GETDATE(), GETDATE()),
+	("francielle4@gmail.com", "$2a$10$wAWbBhyXnJBtQjUpZbDreuTvARojppXD319dMB050LHyAzyXE5hlW", "Francielle", "Vasconcellos Pereira", "69198174037", 4, GETDATE(), GETDATE()),
+	("igor5@gmail.com", "$2a$10$wAWbBhyXnJBtQjUpZbDreuTvARojppXD319dMB050LHyAzyXE5hlW", "Igor", "Vargas Paes", "87205127084", 5, GETDATE(), GETDATE());
